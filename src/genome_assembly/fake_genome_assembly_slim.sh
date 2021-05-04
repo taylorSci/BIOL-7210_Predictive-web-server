b96544c082912be7aa8709136a935337258a7140
James Matthew Hamilton <j.hamilton@gatech.edu>
2021-05-04 05:15:08 -0400

    Added fake genome assembly script. (#53)

---
+++ src/genome_assembly/fake_genome_assembly_slim.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+for last; do true; done
+
+in_dir="${last}"
+out_dir="${last}"
+echo "Output and Input Directory: $in_dir"
+
+# ------------ Unzip All Zips  ------------
+in_zips=($(find "${in_dir}" -name "*.zip"))
+echo $in_zips
+for ii in "${in_zips[@]}"; do
+    echo "Unzipping file: ${ii}"
+    unzip -o -j "${ii}" -d "${in_dir}"
+done
+
+
+# ------------ Get Input Files ------------
+look_for=".fq.gz"
+in_files=($(find "${in_dir}" -name "*${look_for}"))
+basenames=()
+for ii in "${in_files[@]}"; do
+    in_basenames+=($(basename ${ii} "${look_for}"))
+    echo "Input file: ${ii}"
+done
+
+# ------------ Generate Fake Output Files ------------
+
+for ii in "${in_basenames[@]}"; do
+  echo "Generating output files for input file: ${ii}"
+  echo "This is a fake file for testing purposes." > ${out_dir}/${ii}.fasta
+  echo "This is a fake file for testing purposes." > ${out_dir}/${ii}.html
+done
+
