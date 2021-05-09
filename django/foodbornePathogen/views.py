782b56321e98e22e98cc5089d18520d9a939f16d
Anantharaman Shantaraman <ashantaraman3@gatech.edu>
2021-05-08 20:46:43 -0400

    Added skeleton code for email functionality. (#21)

--- django/foodbornePathogen/views.py
+++ django/foodbornePathogen/views.py
@@ -13,13 +13,24 @@ from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.conf import settings
 
-#from email.MIMEMultipart import MIMEMultipart
-#from email.MIMEText import MIMEText
-#from email.MIMEBase import MIMEBase
-#from email import encoders
-
 from .models import *
 from .forms import *
+
+'''
+import smtplib, ssl
+port = 465 # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "fbpservernotify.predict2021@gmail.com" # Enter your address
+receiver_email = "" # Enter receiver address
+password = ""
+message = """\
+Subject: Hi there
+This message is sent from Python, again."""
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+server.login(sender_email, password)
+server.sendmail(sender_email, receiver_email, message)
+'''
 
 logger = logging.getLogger("django")  # /projects/team-1/django/django.log
 
