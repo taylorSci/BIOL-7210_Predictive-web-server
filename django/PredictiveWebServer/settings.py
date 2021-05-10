ae7e03cef385c4dbf8165b6e89683b9ceed3914f
James Matthew Hamilton <j.hamilton@gatech.edu>
2021-05-10 03:10:42 -0400

    Defaulted to using http instead of https. (#61)

--- django/PredictiveWebServer/settings.py
+++ django/PredictiveWebServer/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-BASE_URL = "https://team1.predict2021.biosci.gatech.edu/"
+BASE_URL = "http://team1.predict2021.biosci.gatech.edu/"
 
 STATIC_URL = '/static/'
 
